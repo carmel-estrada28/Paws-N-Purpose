07f848590f77ecdad730fe0e06056853c5fcb476
LAB24_USER ON WS06 <L24Y10W06@LabHCI.net>
2025-10-28 12:15:34 +0800

    setup: created react-app and springboot-project for our project

---
+++ paws-n-purpose-backend/paws-n-purpose-backend/src/test/java/com/kinora/paws_n_purpose_backend/PawsNPurposeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kinora.paws_n_purpose_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawsNPurposeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
