15141a06ac68b896e09a27594284edf75294d70a
MeshaLao <hilarydannelao@gmail.com>
2025-11-20 03:18:30 +0800

    Resolved merge conflict in App.js

--- paws-n-purpose-frontend/src/App.js
+++ paws-n-purpose-frontend/src/App.js
@@ -1,7 +1,8 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from './pages/LoginPage/LoginPage';
 import RegisterPage from './pages/RegisterPage/RegisterPage';
-import Dashboard from './pages/Dashboard';
+import LandingPage from './pages/LandingPage/LandingPage';
+import Dashboard from './pages/Dashboard/Dashboard';
 import './App.css';
 
 function App() {
@@ -11,10 +12,12 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/landing" element={<LandingPage />} />
         <Route path="*" element={<Navigate to="/login" />} />
       </Routes>
     </Router>
   );
+  
 }
 
 export default App;
