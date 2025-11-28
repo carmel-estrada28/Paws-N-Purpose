06f118c867f2bcc3cf61ded9d6794f0b0b2f82ba
Lue <escasinasluezyldominic@gmail.com>
2025-11-28 08:55:00 +0800

    setup: changed spring.jpa.hibernate.ddl-auto to create temporarily so every time the backend recompiles, the database is cleared; added new dependencies

--- paws-n-purpose-backend/paws-n-purpose-backend/src/main/java/com/kinora/paws_n_purpose_backend/PawsNPurposeBackendApplication.java
+++ paws-n-purpose-backend/paws-n-purpose-backend/src/main/java/com/kinora/paws_n_purpose_backend/PawsNPurposeBackendApplication.java
@@ -2,9 +2,6 @@ package com.kinora.paws_n_purpose_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class PawsNPurposeBackendApplication {
@@ -12,10 +9,4 @@ public class PawsNPurposeBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PawsNPurposeBackendApplication.class, args);
 	}
-
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
-
 }
