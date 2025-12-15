788ad6ad12058fe89d6e9ab6a21c4e2764c79b09
Lue <escasinasluezyldominic@gmail.com>
2025-12-15 14:51:56 +0800

    backend: created endpoints to get user's own donations as well as all donations

--- paws-n-purpose-backend/paws-n-purpose-backend/src/main/java/com/kinora/paws_n_purpose_backend/repository/DonationBoxRepository.java
+++ paws-n-purpose-backend/paws-n-purpose-backend/src/main/java/com/kinora/paws_n_purpose_backend/repository/DonationBoxRepository.java
@@ -1,11 +1,17 @@
 package com.kinora.paws_n_purpose_backend.repository;
 
+
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.kinora.paws_n_purpose_backend.entity.DonationBox;
 
 @Repository
 public interface DonationBoxRepository extends JpaRepository<DonationBox, Long>{
-
+    List<DonationBox> findByOwnerId(Long ownerId);
+    List<DonationBox> findByOwnerId(Long ownerId, Pageable pageable);
 }
